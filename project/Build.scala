6b9d2161b81653fee06daf9b62be934de074837c
Razvan Cojocaru <pub@razie.com>
2016-05-06 09:43:53 -0400

    updated to scala 2.11

--- project/Build.scala
+++ project/Build.scala
@@ -3,25 +3,27 @@ import Keys._
 import java.io.File
 
 object V {
-  val version      = "0.6.4-SNAPSHOT"
-  val scalaVersion = "2.10.0" // "2.9.1"
+  val version      = "0.9.1"//-SNAPSHOT"
+  val scalaVersion = "2.11.8"
   val organization = "com.razie"
 
   def snap = (if (V.version endsWith "-SNAPSHOT") "-SNAPSHOT" else "")
 
-  def RAZBASEVER = "0.6.4" + snap
+  def RAZBASEVER = "0.9.1" + snap
 }
 
 object MyBuild extends Build {
 
-  def scalatest  = "org.scalatest"  % "scalatest_2.10.0" % "1.8"
-  def junit      = "junit"          % "junit"           % "4.5" //     % "test->default"
-  def json       = "org.json"       % "json"            % "20090211"
-  def slf4jApi   = "org.slf4j"      % "slf4j-api"       % "1.6.1"
-  def logback    = "ch.qos.logback" % "logback-classic" % "1.0.0"
+  def scalatest  = "org.scalatest"  %% "scalatest"       % "2.1.3"
+  def junit      = "junit"           % "junit"           % "4.5" //     % "test->default"
+  def json       = "org.json"        % "json"            % "20090211"
+  def slf4jApi   = "org.slf4j"       % "slf4j-api"       % "1.6.1"
+  def logback    = "ch.qos.logback"  % "logback-classic" % "1.0.0"
   
-  def razBase    = "com.razie" %% "razbase"         % V.RAZBASEVER
+//  def razBase    = "com.razie" %% "base"            % V.RAZBASEVER
+  def razBase    = "com.razie" %% "razweb"          % V.RAZBASEVER
   def w20        = "com.razie" %% "s20widgets"      % V.RAZBASEVER
+  def snakked    = "com.razie" %% "snakk-core"      % V.RAZBASEVER
 
   lazy val root = Project(id="lightsoa",    base=file("."),
                           settings = defaultSettings ++ Seq()
@@ -29,7 +31,7 @@ object MyBuild extends Build {
 
   lazy val core = Project(id="lightsoa-core", base=file("core"),
                           settings = defaultSettings ++ 
-                          Seq(libraryDependencies ++= Seq(scalatest, junit, json, razBase, w20))
+                          Seq(libraryDependencies ++= Seq(scalatest, junit, json, razBase, w20, snakked))
                   )
 
   def defaultSettings = baseSettings ++ Seq()
@@ -40,16 +42,18 @@ object MyBuild extends Build {
     organization         := V.organization,
     organizationName     := "Razie's Pub",
     organizationHomepage := Some(url("http://www.razie.com")),
+    licenses := Seq("BSD-style" -> url("http://www.opensource.org/licenses/bsd-license.php")),
+    homepage := Some(url("http://www.razie.com")),
 
     publishTo <<= version { (v: String) =>
       if(v endsWith "-SNAPSHOT")
         Some ("Sonatype" at "https://oss.sonatype.org/content/repositories/snapshots/")
       else
-        Some ("Sonatype" at "https://oss.sonatype.org/content/repositories/releases/")
+        Some ("Sonatype" at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
     } ,
 
-    resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
-                      "releases"  at "http://oss.sonatype.org/content/repositories/releases")
+    resolvers ++= Seq("snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+                      "releases"  at "https://oss.sonatype.org/content/repositories/releases")
     )
 
 }
